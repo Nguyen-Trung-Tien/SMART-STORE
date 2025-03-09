645ef71d6c9618caf6d07fcbab57afc0a8743d2f
TrungTien2004_@UTH <118372166+Nguyen-Trung-Tien@users.noreply.github.com>
2025-03-09 19:08:39 +0700

    update create user

--- backend/src/services/UserService.js
+++ backend/src/services/UserService.js
@@ -3,6 +3,10 @@ const createUser = (newUser) => {
   return new Promise(async (resolve, reject) => {
     const { name, email, password, confirmPassword, phone } = newUser;
     try {
+      const checkUser = await User.findOne({ email: email });
+      if (checkUser !== null) {
+        resolve({ status: "OK", message: "The email is already" });
+      }
       const createUser = await User.create({
         name,
         email,
