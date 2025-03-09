a71591238e8a5ed0f7e4b2bfe775c1b8b73aa41f
TrungTien2004_@UTH <118372166+Nguyen-Trung-Tien@users.noreply.github.com>
2025-03-09 09:58:24 +0700

    update model: user, product, orders and connect with mogoosedb, server

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,14 +1,21 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.get("/", (req, res) => {
-  res.send("Hello World everyone!");
-});
+mongoose
+  .connect(
+    `mongodb+srv://project-ecommer:${process.env.MONGO_DB}@cluster0.1lnoi.mongodb.net/project_web?retryWrites=true&w=majority&appName=Cluster0`
+  )
+  .then(() => {
+    console.log("Connected to MongoDB successfully");
+  })
+  .catch((error) => {});
+
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
