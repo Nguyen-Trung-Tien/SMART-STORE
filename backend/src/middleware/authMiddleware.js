de5316b0694e0cfe6256038231b1589c468902f8
TrungTien2004_@UTH <118372166+Nguyen-Trung-Tien@users.noreply.github.com>
2025-03-25 15:12:39 +0700

    Build UI for admin page

--- backend/src/middleware/authMiddleware.js
+++ backend/src/middleware/authMiddleware.js
@@ -1,16 +1,17 @@
 const jwt = require("jsonwebtoken");
 const dotenv = require("dotenv");
+const e = require("express");
 dotenv.config();
 
 const authMiddleware = (req, res, next) => {
   const token = req.headers.token.split(" ")[1];
   jwt.verify(token, process.env.ACCESS_TOKEN, function (err, user) {
-    if (err) {
-      return res.status(404).json({
-        message: "The authentication",
-        status: "ERROR",
-      });
-    }
+    // if (err) {
+    //   return res.status(404).json({
+    //     message: "The authentication",
+    //     status: "ERROR",
+    //   });
+    // }
     if (user?.isAdmin) {
       next();
     } else {
@@ -26,12 +27,12 @@ const authUserMiddleware = (req, res, next) => {
   const token = req.headers.token.split(" ")[1];
   const userId = req.params.id;
   jwt.verify(token, process.env.ACCESS_TOKEN, function (err, user) {
-    if (err) {
-      return res.status(404).json({
-        message: "The authentication",
-        status: "ERROR",
-      });
-    }
+    // if (err) {
+    //   return res.status(404).json({
+    //     message: "The authentication",
+    //     status: "ERROR",
+    //   });
+    // }
     if (user?.isAdmin || user?.id === userId) {
       next();
     } else {
---
+++ frontend/src/components/AdminProduct/AdminProduct.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { WrapperHeader } from "./style";
+import { Button } from "antd";
+import { PlusOutlined } from "@ant-design/icons";
+import TableComponent from "../TableComponent/TableComponent";
+const AdminProduct = () => {
+  return (
+    <div>
+      <WrapperHeader>Quản lý sản phẩm</WrapperHeader>
+      <div style={{ marginTop: "20px" }}>
+        <Button
+          style={{
+            height: "100px",
+            width: "100px",
+            borderRadius: "10px",
+            borderStyle: "dashed",
+          }}
+        >
+          <PlusOutlined style={{ fontSize: "60px" }} />
+        </Button>
+      </div>
+      <div style={{ marginTop: "20px" }}>
+        <TableComponent />
+      </div>
+    </div>
+  );
+};
+
+export default AdminProduct;
