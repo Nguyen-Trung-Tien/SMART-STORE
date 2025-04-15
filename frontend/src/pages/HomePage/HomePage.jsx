ebe21a90a3d5e637e1d3a47deba22130377b65b9
TrungTien2004_@UTH <118372166+Nguyen-Trung-Tien@users.noreply.github.com>
2025-04-15 16:51:24 +0700

    .

--- frontend/src/pages/HomePage/HomePage.jsx
+++ frontend/src/pages/HomePage/HomePage.jsx
@@ -7,7 +7,7 @@ import {
 import SliderComponent from "../../components/SliderComponent/SliderComponent";
 import image1 from "../../assets/images/image-1.png";
 import image2 from "../../assets/images/image-2.png";
-import image3 from "../../assets/images/image3.webp";
+import image3 from "../../assets/images/img-3.png";
 import CardComponent from "../../components/CardComponent/CardComponent";
 import TypeProducts from "../../components/TypeProducts/TypeProducts";
 import { useQuery } from "@tanstack/react-query";
